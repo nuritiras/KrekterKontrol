ae952d7f589a1a84f0d24cebd2c6b06709c2773b
nuri <nuri@ROGRT>
2024-02-13 12:05:20 +0300

    Proje dosyası ekle.

---
+++ KrekterKontrol/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace KrekterKontrol
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::KrekterKontrol.Properties.Resources.indir;
+            this.pictureBox1.Location = new System.Drawing.Point(242, 88);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(198, 263);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1279, 604);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ KrekterKontrol/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KrekterKontrol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //picturebox'a x ve y koordinatları atıyoruz.
+            int x = pictureBox1.Location.X;
+            int y = pictureBox1.Location.Y;
+
+            if (e.KeyCode == Keys.Right && x>0)
+                x += 5;//Tuşa 1 kere basıldığında kaç pixel kayacağını gösteriyor
+            if (e.KeyCode == Keys.Left && x>0)
+                x -= 5;//Tuşa 1 kere basıldığında kaç pixel kayacağını gösteriyor
+            else if (e.KeyCode == Keys.Up && y > 0)
+                y -= 5;
+            else if (e.KeyCode == Keys.Down && y < 410)
+                y += 5;
+            pictureBox1.Location = new Point(x, y);
+
+            if (x < 100 && y < 100)// Nesne sol üste gidince işlem yapılsın diye.
+            {
+                MessageBox.Show("Tebriklern:");
+                pictureBox1.Location = new Point(250, 250);//Sol üste gidince tekrar başlıyacağı yer.
+            }
+        }
+    }
+}
